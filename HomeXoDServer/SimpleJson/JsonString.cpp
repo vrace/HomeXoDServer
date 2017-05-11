b753927a01c3ca5f0d1d09f23694e185bb272e94
wane <vrace_studios@hotmail.com>
2017-05-11 22:44:14 +0800

    JsonString deal with double quote and escape

--- HomeXoDServer/SimpleJson/JsonString.cpp
+++ HomeXoDServer/SimpleJson/JsonString.cpp
@@ -24,7 +24,22 @@ JsonString::operator const std::string&() const
 
 std::string JsonString::ToString() const
 {
-	return "\"" + _str + "\"";
+	std::string dup = _str;
+	std::string::size_type pos = 0;
+
+	for (pos = dup.find('\\'); pos != std::string::npos; pos = dup.find('\\', pos))
+	{
+		dup.replace(pos, 1, "\\\\");
+		pos += 2;
+	}
+
+	for (pos = dup.find('"'); pos != std::string::npos; pos = dup.find('"', pos))
+	{
+		dup.replace(pos, 1, "\\\"");
+		pos += 2;
+	}
+
+	return "\"" + dup + "\"";
 }
 
 bool JsonString::operator <(const JsonString &rhs) const
