2c66ce7b035cb2e669b65536d320046d3726a0b5
wane <vrace_studios@hotmail.com>
2017-05-16 21:17:19 +0800

    Change media list to use more cleaner response

--- HomeXoDServer/Base/HttpResponseEntity.h
+++ HomeXoDServer/Base/HttpResponseEntity.h
@@ -13,10 +13,22 @@ public:
 
 	}
 
+	HttpResponseEntity(HttpStatusCode statusCode, EntityType *entity)
+		: HttpResponse(HttpStatus(statusCode), HttpHeaderWithContentType(entity->GetContentType()), entity->ToString())
+	{
+		delete entity;
+	}
+
 	HttpResponseEntity(HttpStatusCode statusCode, const EntityType &entity, const std::string &contentType)
 		: HttpResponse(HttpStatus(statusCode), HttpHeaderWithContentType(contentType), entity.ToString())
 	{
 
+	}
+
+	HttpResponseEntity(HttpStatusCode statusCode, const EntityType *entity, const std::string &contentType)
+		: HttpResponse(HttpStatus(statusCode), HttpHeaderWithContentType(contentType), entity->ToString())
+	{
+		delete entity;
 	}
 };
 
